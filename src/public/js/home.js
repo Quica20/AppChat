3100bb2d43251fc18891697ca7b64ca4c74fac84
tomas quicaño <tomquica@gmail.com>
2023-06-07 17:37:02 -0300

    first commit

---
+++ src/public/js/home.js
@@ -0,0 +1,45 @@
+const socket = io()
+let user;
+
+const chatbox = document.getElementById("chatbox");
+const messageLogs = document.getElementById("messageLogs");
+
+Swal.fire({
+    title: 'Identificate',
+    input: 'text',
+    inputValidator: (value) => {
+        return !value && 'Necesitas escribir un nombre de USUARIO'
+    },
+    allowOutsideClick: false,
+}).then(result => {
+    user = result.value;
+    socket.emit('authenticatedUser', user)
+})
+
+chatbox.addEventListener("keyup", (evt) => {
+    if (evt.key === "Enter") {
+        socket.emit("message", { user: user, message: chatbox.value });
+        chatbox.value = "";
+    };
+});
+
+socket.on("imprimir", (data) => {
+    let mensajes = '';
+    data.forEach(msj => {
+        mensajes += `${msj.user} Escribio: ${msj.message} <br/>`
+    })
+    messageLogs.innerHTML = mensajes
+});
+
+socket.on('newUserAlert', (data) => {
+    if (!user) return
+    Swal.fire({
+        toast: true,
+        position: 'top-end',
+        showConfirmButton: false,
+        timer: 5000,
+        title: data + 'Se ha unido al chat',
+        icon: 'success'
+    })
+});
+
